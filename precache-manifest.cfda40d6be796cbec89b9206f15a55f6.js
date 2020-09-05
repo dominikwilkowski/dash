135df241007ac079069d2bae417a19e88c1e41c8
Dominik Wilkowski <hi@dominik-wilkowski.com>
2020-09-05 21:12:09 +1000

    Updates

---
+++ precache-manifest.cfda40d6be796cbec89b9206f15a55f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d66928fd925fbfaea52768d1ee9d042",
+    "url": "/dash/index.html"
+  },
+  {
+    "revision": "db16701471e3ffa56738",
+    "url": "/dash/static/js/2.406b3b85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dash/static/js/2.406b3b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d7cda53d7478ce421b",
+    "url": "/dash/static/js/main.f26c2761.chunk.js"
+  },
+  {
+    "revision": "942e7e294bca19aab8e2",
+    "url": "/dash/static/js/runtime-main.6922e2ea.js"
+  }
+]);
