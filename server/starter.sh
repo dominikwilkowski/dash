38d1196727452be9797fdf47db1af09075ede374
Dominik Wilkowski <hi@dominik-wilkowski.com>
2021-06-02 21:40:37 +1000

    fixed shell script

--- server/starter.sh
+++ server/starter.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
-then
-	export PATH=/usr/local/bin:$PATH
-	forever start -l dash.log --workingDir /www/dash/ --append -o dashOut.log -e dashError.log /www/dash/dashServer.js serve >> /home/deploy/.forever/dash-apiRestart.log 2>&1
-fi
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+forever start -l dash.log --workingDir /www/dash/ --append -o dashOut.log -e dashError.log /www/dash/dashServer.js serve >> /home/deploy/.forever/dash-apiRestart.log 2>&1
